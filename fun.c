f9125414cde950d61df25133dcc8bf822cbdf4a5
Ron Popov <DirtySlowpoke@gmail.com>
2024-01-11 03:24:45 +0200

    Interesting interactive cli

---
+++ fun.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/ptrace.h>
+#include <sys/user.h>
+
+int main( int argc, char *argv[] )  {
+    printf("[v] Linux debugger init\n");
+
+    int debugged_process_pid;
+
+    // Should get 1 param (pid)
+    if(argc == 1) {
+        printf("[x] Not enough parameters passed!\n");
+        printf("[x] Usage: ./debugger <pid>\n");
+        return 0;
+    } else if(argc == 2) {
+        debugged_process_pid = atoi(argv[1]);
+        printf("[-] Debugged process PID: %d\n", debugged_process_pid);
+    } else {
+        printf("[x] Too much arguments passed\n");
+        printf("[x] Usage: ./debugger <pid>\n");
+        return 0;
+    }
+
+
+    // PTRACE_ATTACH the debugged process
+    long ptrace_attach_ret_val = ptrace(PTRACE_ATTACH, debugged_process_pid, 0, 0);
+    if(ptrace_attach_ret_val == -1)
+        printf("[x] An error occured in PTRACE_ATTACH: %s\n", strerror(errno));
+    else
+        printf("[-] PTRACE_ATTACH return value: %ld\n", ptrace_attach_ret_val);
+
+
+    // Get current RIP - instruction pointer
+    long unsigned int target_addr = __builtin_offsetof(struct user, regs.rip);
+    long debugged_process_ip = ptrace(PTRACE_PEEKUSER, debugged_process_pid, target_addr, 0);
+    if(debugged_process_ip == -1) {
+        printf("[x] An error occured in PTRACE_PEEKUSER: %s\n", strerror(errno));
+    } else {
+        printf("[-] PTRACE_PEEKUSER return value: %ld\n", debugged_process_ip);
+    }
+
+    // Check current instruction
+    long current_instruction = ptrace(PTRACE_PEEKDATA, debugged_process_pid, debugged_process_ip, 0);
+    if(current_instruction == -1) {
+        printf("[x] An error occured in PTRACE_PEEKDATA: %s\n", strerror(errno));
+    } else {
+        printf("[-] PTRACE_PEEKDATA return value: 0x%08lx\n", current_instruction);
+    }
+
+
+
+    // Detach from process
+    long ptrace_detach_return_value = ptrace(PTRACE_DETACH, debugged_process_pid, 0);
+    if(ptrace_detach_return_value == -1) {
+        printf("[x] An error occured in PTRACE_DETACH: %s\n", strerror(errno));
+    } else {
+        printf("[-] PTRACE_DETACH return value: %ld\n", ptrace_detach_return_value);
+    }
+
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -5,59 +6,81 @@
 #include <sys/ptrace.h>
 #include <sys/user.h>
 
+#define STRING_MAX_LEN 1024
+
+#define equals(debugger_command,b) strcmp(debugger_command,b) == 0
+
 int main( int argc, char *argv[] )  {
-    printf("[v] Linux debugger init\n");
+    printf("--- Linux debugger init ---\n");
 
-    int debugged_process_pid;
-
-    // Should get 1 param (pid)
-    if(argc == 1) {
-        printf("[x] Not enough parameters passed!\n");
-        printf("[x] Usage: ./debugger <pid>\n");
-        return 0;
-    } else if(argc == 2) {
-        debugged_process_pid = atoi(argv[1]);
-        printf("[-] Debugged process PID: %d\n", debugged_process_pid);
-    } else {
-        printf("[x] Too much arguments passed\n");
-        printf("[x] Usage: ./debugger <pid>\n");
+    // Verify i am root
+    if(getuid() != 0)
+    {
+        printf("[X] YOU ARE NOT ROOT!\n");
         return 0;
     }
 
+    printf("[-] Type \"exit\" to exit\n");
+    char raw_user_input[STRING_MAX_LEN];
 
-    // PTRACE_ATTACH the debugged process
-    long ptrace_attach_ret_val = ptrace(PTRACE_ATTACH, debugged_process_pid, 0, 0);
-    if(ptrace_attach_ret_val == -1)
-        printf("[x] An error occured in PTRACE_ATTACH: %s\n", strerror(errno));
-    else
-        printf("[-] PTRACE_ATTACH return value: %ld\n", ptrace_attach_ret_val);
+    long unsigned int working_pid = 0;
 
+    // Get user input
+    while (NULL == NULL)
+    {
+        printf(">>> ");
+        fgets(raw_user_input, STRING_MAX_LEN, stdin);
 
-    // Get current RIP - instruction pointer
-    long unsigned int target_addr = __builtin_offsetof(struct user, regs.rip);
-    long debugged_process_ip = ptrace(PTRACE_PEEKUSER, debugged_process_pid, target_addr, 0);
-    if(debugged_process_ip == -1) {
-        printf("[x] An error occured in PTRACE_PEEKUSER: %s\n", strerror(errno));
-    } else {
-        printf("[-] PTRACE_PEEKUSER return value: %ld\n", debugged_process_ip);
-    }
+        
+        char* user_input = strtok(raw_user_input, "\n");
+        char* debugger_command = strtok(user_input, " ");
+        // printf("Debugger command is : \"%s\"\n", debugger_command);
 
-    // Check current instruction
-    long current_instruction = ptrace(PTRACE_PEEKDATA, debugged_process_pid, debugged_process_ip, 0);
-    if(current_instruction == -1) {
-        printf("[x] An error occured in PTRACE_PEEKDATA: %s\n", strerror(errno));
-    } else {
-        printf("[-] PTRACE_PEEKDATA return value: 0x%08lx\n", current_instruction);
-    }
+        if(equals(debugger_command, "exit")) {
+            printf("[-] Leaving, bye!\n");
+            return 0;
 
+        } else if (equals(debugger_command, "help")) {
+            printf("\n--- Linux Debugger Help Page ---\n");
+            printf("  help              - Display the help page\n");
+            printf("  pid <pid>         - Set\\Get the pid of the process to work on\n");
+            printf("  attach            - attaches to the process\n");
+            printf("  detach            - detach from the process\n");
+            printf("\n");
 
+        } else if (equals(debugger_command, "attach")) {
+            long attach_ret_val = ptrace(PTRACE_ATTACH, working_pid, 0, 0);
+            if(attach_ret_val == -1)
+                printf("[X] An error occured in PTRACE_ATTACH: %s\n", strerror(errno));
+            else {
+                printf("[-] Attach return value: %ld\n", attach_ret_val);
+                printf("[V] Attached succesfully!\n");
+            }
 
-    // Detach from process
-    long ptrace_detach_return_value = ptrace(PTRACE_DETACH, debugged_process_pid, 0);
-    if(ptrace_detach_return_value == -1) {
-        printf("[x] An error occured in PTRACE_DETACH: %s\n", strerror(errno));
-    } else {
-        printf("[-] PTRACE_DETACH return value: %ld\n", ptrace_detach_return_value);
+        } else if (equals(debugger_command, "detach")) {
+            long detach_ret_val = ptrace(PTRACE_DETACH, working_pid, 0, 0);
+            if(detach_ret_val == -1)
+                printf("[X] An error occured in PTRACE_DETACH: %s\n", strerror(errno));
+            else {
+                printf("[-] Detach return value: %ld\n", detach_ret_val);
+                printf("[V] Detached succesfully!\n");
+            }
+
+        } else if (equals(debugger_command, "pid")) {
+            // Passing NULL to strtok just continues iterating over the last
+            // string that strtok was called on, kinda weird but works
+            char* pid_string = strtok(NULL, " ");
+            if (pid_string == NULL || equals(pid_string, "")) {
+                if (working_pid == 0) {
+                    printf("[-] Working pid not set\n");
+                } else {
+                    printf("[-] Working pid is : %ld\n", working_pid);
+                }
+            } else {
+                int pid = atoi(pid_string);
+                working_pid = pid;
+            }
+        }
     }
 
     return 0;
