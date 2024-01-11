ebf56ccd020b63648e2af969a1d65b3478b8f293
Ron Popov <DirtySlowpoke@gmail.com>
2024-01-12 01:22:12 +0200

    Print bytes in addr of current instruction pointer

--- main.c
+++ main.c
@@ -20,10 +20,31 @@ int main( int argc, char *argv[] )  {
         return 0;
     }
 
-    printf("[-] Type \"exit\" to exit\n");
     char raw_user_input[STRING_MAX_LEN];
 
     long unsigned int working_pid = 0;
+
+    if (argc == 2) {
+        unsigned long debugged_process_pid = atoi(argv[1]);
+        printf("[-] Debugged process PID: %ld\n", debugged_process_pid);
+
+        if (debugged_process_pid == 0) {
+            printf("[X] Invalid pid provided in cli argument\n");
+            return 0;
+        }
+
+        long attach_ret_val = ptrace(PTRACE_ATTACH, debugged_process_pid, 0, 0);
+        if(attach_ret_val == -1) {
+            printf("[X] An error occured in PTRACE_ATTACH: %s\n", strerror(errno));
+            return 0;
+        }
+
+        printf("[-] Attach return value: %ld\n", attach_ret_val);
+        printf("[V] Attached succesfully!\n");
+        working_pid = debugged_process_pid;
+    }
+
+    printf("[-] Type \"exit\" to exit\n");
 
     // Get user input
     while (NULL == NULL)
@@ -49,6 +70,7 @@ int main( int argc, char *argv[] )  {
             printf("  help              - Display the help page\n");
             printf("  attach <pid>      - attaches to the specified\n");
             printf("  detach            - detach from currently attached process\n");
+            printf("  diss              - Print instruction pointer and current instructions\n");
             printf("\n");
 
         } else if (equals(debugger_command, "attach")) {
@@ -73,6 +95,45 @@ int main( int argc, char *argv[] )  {
                 printf("[V] Detached succesfully!\n");
                 working_pid = 0;
             }
+
+        } else if (equals(debugger_command, "diss")) {
+            // Make sure there is a process attached by the debugger
+            if (working_pid == 0) {
+                printf("[X] Please attach debugger to a process before trying to disassemble!\n");
+                continue;
+            }
+
+
+            // Find value of RIP - instruction pointer
+            long unsigned int rip_addr_in_user_section = __builtin_offsetof(struct user, regs.rip);
+            long debugged_process_ip = ptrace(PTRACE_PEEKUSER, working_pid, rip_addr_in_user_section, 0);
+            if(debugged_process_ip == -1) {
+                printf("[x] An error occured when finding RIP: %s\n", strerror(errno));
+                continue;
+            }
+
+            for(int i = 0; i < 8; i++) {
+                // Check current instruction
+                long targer_addr = debugged_process_ip + i*4;
+                long instructions = ptrace(PTRACE_PEEKDATA, working_pid, targer_addr, 0);
+                if(instructions == -1) {
+                    printf("[x] An error occured in finding current instrucion: %s\n", strerror(errno));
+                    continue;
+                } else {
+                    // printf("[-] PTRACE_PEEKDATA for current instruction return value: 0x%08lx\n", instructions);
+                    printf(
+                        "0x%08lx - 0x%02X 0x%02X 0x%02X 0x%02X\n", 
+                        targer_addr, 
+                        (unsigned int) (instructions % 0x0100), 
+                        (unsigned int) (instructions % 0x010000 / 0x0100),
+                        (unsigned int) (instructions % 0x01000000 / 0x010000),
+                        (unsigned int) (instructions % 0x0100000000 / 0x01000000)
+                        );
+                }
+
+            }
+
+            
         } else {
             printf("[X] Unknown command : \"%s\"\n", debugger_command);
         }
