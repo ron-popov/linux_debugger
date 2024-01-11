e903485a29bcbf5d9094115783da60271b98188f
Ron Popov <DirtySlowpoke@gmail.com>
2024-01-11 17:06:55 +0200

    Cleaner experience

--- main.c
+++ main.c
@@ -28,13 +28,17 @@ int main( int argc, char *argv[] )  {
     // Get user input
     while (NULL == NULL)
     {
-        printf(">>> ");
+        if (working_pid == 0) {
+            printf(">>> ");
+        } else {
+            printf("(%ld) >>> ", working_pid);
+        }
+
         fgets(raw_user_input, STRING_MAX_LEN, stdin);
 
         
         char* user_input = strtok(raw_user_input, "\n");
         char* debugger_command = strtok(user_input, " ");
-        // printf("Debugger command is : \"%s\"\n", debugger_command);
 
         if(equals(debugger_command, "exit")) {
             printf("[-] Leaving, bye!\n");
@@ -43,18 +47,21 @@ int main( int argc, char *argv[] )  {
         } else if (equals(debugger_command, "help")) {
             printf("\n--- Linux Debugger Help Page ---\n");
             printf("  help              - Display the help page\n");
-            printf("  pid <pid>         - Set\\Get the pid of the process to work on\n");
-            printf("  attach            - attaches to the process\n");
-            printf("  detach            - detach from the process\n");
+            printf("  attach <pid>      - attaches to the specified\n");
+            printf("  detach            - detach from currently attached process\n");
             printf("\n");
 
         } else if (equals(debugger_command, "attach")) {
-            long attach_ret_val = ptrace(PTRACE_ATTACH, working_pid, 0, 0);
+            char* pid_string = strtok(NULL, " ");
+            int pid = atoi(pid_string);
+
+            long attach_ret_val = ptrace(PTRACE_ATTACH, pid, 0, 0);
             if(attach_ret_val == -1)
                 printf("[X] An error occured in PTRACE_ATTACH: %s\n", strerror(errno));
             else {
                 printf("[-] Attach return value: %ld\n", attach_ret_val);
                 printf("[V] Attached succesfully!\n");
+                working_pid = pid;
             }
 
         } else if (equals(debugger_command, "detach")) {
@@ -64,22 +71,10 @@ int main( int argc, char *argv[] )  {
             else {
                 printf("[-] Detach return value: %ld\n", detach_ret_val);
                 printf("[V] Detached succesfully!\n");
+                working_pid = 0;
             }
-
-        } else if (equals(debugger_command, "pid")) {
-            // Passing NULL to strtok just continues iterating over the last
-            // string that strtok was called on, kinda weird but works
-            char* pid_string = strtok(NULL, " ");
-            if (pid_string == NULL || equals(pid_string, "")) {
-                if (working_pid == 0) {
-                    printf("[-] Working pid not set\n");
-                } else {
-                    printf("[-] Working pid is : %ld\n", working_pid);
-                }
-            } else {
-                int pid = atoi(pid_string);
-                working_pid = pid;
-            }
+        } else {
+            printf("[X] Unknown command : \"%s\"\n", debugger_command);
         }
     }
 
