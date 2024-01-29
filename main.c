dc9ed6f944da1faf23079a7bedb83b7b8942161f
Ron Popov <DirtySlowpoke@gmail.com>
2024-01-29 10:52:04 -0500

    Did some order in types used and maybe initialized breakpoint

--- main.c
+++ main.c
@@ -5,10 +5,48 @@
 #include <string.h>
 #include <sys/ptrace.h>
 #include <sys/user.h>
+#include <sys/wait.h>
+#include <stdbool.h>
 
 #define STRING_MAX_LEN 1024
+#define BREAKPOINT_OPCODE 0x33
 
 #define equals(a,b) strcmp(a,b) == 0
+
+typedef long unsigned int PID;
+typedef unsigned long long ADDR;
+typedef unsigned char BYTE;
+typedef unsigned long long QWORD;
+
+BYTE read_mem(PID pid, ADDR addr) {
+    QWORD result = ptrace(PTRACE_PEEKDATA, pid, addr, 0);
+
+    if(result == -1) {
+        printf("[x] An error occured during read for read: %s\n", strerror(errno));
+        exit(0);
+    }
+
+    return result % 0x0100;
+}
+
+void write_mem(PID pid, ADDR addr, BYTE value) {
+    QWORD result = ptrace(PTRACE_PEEKDATA, pid, addr, 0);
+
+    if(result == -1) {
+        printf("[x] An error occured during read for write: %s\n", strerror(errno));
+        exit(0);
+    }
+
+    result = result & 0xFFFFFF00;
+    result += value;
+
+    QWORD write_mem_result = ptrace(PTRACE_POKEDATA, pid, addr, result);
+    if (write_mem_result != 0) {
+        printf("[x] An error occured during write for write: %s\n", strerror(errno));
+        exit(0);
+    }
+
+}
 
 int main( int argc, char *argv[] )  {
     printf("--- Linux debugger init ---\n");
@@ -20,7 +58,7 @@ int main( int argc, char *argv[] )  {
         return 0;
     }
 
-    long unsigned int working_pid = 0;
+    PID working_pid = 0;
 
     char raw_user_input[STRING_MAX_LEN];
     printf("[-] Type \"exit\" to exit\n");
@@ -59,7 +97,7 @@ int main( int argc, char *argv[] )  {
             char* pid_string = strtok(NULL, " ");
             int pid = atoi(pid_string);
 
-            long attach_ret_val = ptrace(PTRACE_ATTACH, pid, 0, 0);
+            QWORD attach_ret_val = ptrace(PTRACE_ATTACH, pid, 0, 0);
             if(attach_ret_val == -1)
                 printf("[X] An error occured in PTRACE_ATTACH: %s\n", strerror(errno));
             else {
@@ -74,7 +112,10 @@ int main( int argc, char *argv[] )  {
                 continue;
             }
 
-            long detach_ret_val = ptrace(PTRACE_DETACH, working_pid, 0, 0);
+            // TODO : If process is defunct than detach will fail but the process is not dead
+            // So it has to be killed by us
+
+            QWORD detach_ret_val = ptrace(PTRACE_DETACH, working_pid, 0, 0);
             if(detach_ret_val == -1)
                 printf("[X] An error occured in PTRACE_DETACH: %s\n", strerror(errno));
             else {
@@ -94,17 +135,17 @@ int main( int argc, char *argv[] )  {
 
             printf("[-] Command to execute is : \"%s %s\"\n", shell_command, arguments);
 
-            long fork_ret_val = fork();
+            QWORD fork_ret_val = fork();
 
             if (fork_ret_val == 0) {
                 // This is the forked process, i am not doing anything with the return value
                 // Because i have nothing to do with it (this is the forked process)
 
                 // Disable stdout for forked process
-                // freopen("/dev/null", "w", stdout);
+                freopen("/dev/null", "w", stdout);
 
                 // TRACE_ME
-                long traceme_ret_val = ptrace(PTRACE_TRACEME, PTRACE_O_TRACEEXEC);                
+                QWORD traceme_ret_val = ptrace(PTRACE_TRACEME);                
 
                 // EXECVE
                 // TODO: Implement support for command line arguments
@@ -117,19 +158,20 @@ int main( int argc, char *argv[] )  {
             } else {
                 // This is the original flow
 
-                // Trace the process, it should have executed traceme so we could trace it
-                long attach_ret_val = ptrace(PTRACE_ATTACH, fork_ret_val, 0, 0);
-                if(attach_ret_val == -1)
-                    printf("[X] An error occured in PTRACE_ATTACH: %s\n", strerror(errno));
-                else {
-                    printf("[V] Attached succesfully!\n");
-                    working_pid = fork_ret_val;
+                // waitpid for process to settle down
+                // Make sure it is in expected state
+                int wait_pid_status = 0;
+
+                waitpid(fork_ret_val, &wait_pid_status, 0);
+                printf("[-] waitpid status : %d\n", wait_pid_status);
+
+                if (!(WIFSTOPPED(wait_pid_status) && WSTOPSIG(wait_pid_status) == SIGTRAP)) {
+                    printf("[X] Process is in weird state...\n");
                 }
 
-                long set_options_ret_val = ptrace(PTRACE_SETOPTIONS, fork_ret_val, 0, PTRACE_O_TRACEEXEC);
-                printf("[-] PTRACE SETOPTIONS return val : %d\n", set_options_ret_val);
-
-                printf("[x] An error occured when settings ptrace options: %s\n", strerror(errno));
+                printf("[-] WIFSTOPPED : %d\n", WIFSTOPPED(wait_pid_status));
+                printf("[-] WSTOPSIG : %d\n", WSTOPSIG(wait_pid_status));
+                working_pid = fork_ret_val;
             }
 
         } else if (equals(debugger_command, "diss")) {
@@ -141,24 +183,27 @@ int main( int argc, char *argv[] )  {
 
 
             // Find value of RIP - instruction pointer
-            long unsigned int rip_addr_in_user_section = __builtin_offsetof(struct user, regs.rip);
-            long debugged_process_ip = ptrace(PTRACE_PEEKUSER, working_pid, rip_addr_in_user_section, 0);
+            PID rip_addr_in_user_section = __builtin_offsetof(struct user, regs.rip);
+            QWORD debugged_process_ip = ptrace(PTRACE_PEEKUSER, working_pid, rip_addr_in_user_section, 0);
             if(debugged_process_ip == -1) {
                 printf("[x] An error occured when finding RIP: %s\n", strerror(errno));
                 continue;
             }
 
+
             for(int i = 0; i < 8; i++) {
                 // Check current instruction
-                long targer_addr = debugged_process_ip + i*4;
-                unsigned long instructions = ptrace(PTRACE_PEEKDATA, working_pid, targer_addr, 0);
+                QWORD target_addr = debugged_process_ip + i*4;
+                printf("[-] Reading addr : %d\n", target_addr);
+
+                QWORD instructions = ptrace(PTRACE_PEEKDATA, working_pid, target_addr, 0);
                 if(instructions == -1) {
                     printf("[x] An error occured in finding current instrucion: %s\n", strerror(errno));
                     continue;
                 } else {
                     printf(
                         "0x%08lx - 0x%02X 0x%02X 0x%02X 0x%02X\n", 
-                        targer_addr, 
+                        target_addr, 
                         (unsigned int) (instructions % 0x0100), 
                         (unsigned int) (instructions % 0x010000 / 0x0100),
                         (unsigned int) (instructions % 0x01000000 / 0x010000),
@@ -167,6 +212,17 @@ int main( int argc, char *argv[] )  {
                 }
 
             }
+        } else if (equals(debugger_command, "break")) {
+            // Make sure there is a process attached by the debugger
+            if (working_pid == 0) {
+                printf("[X] Please attach debugger to a process before trying to set breakpoints!\n");
+                continue;
+            }
+
+            char* breakpoint_addr_string = strtok(NULL, " ");
+            ADDR breakpoint_addr = atoi(breakpoint_addr_string);
+
+            write_mem(working_pid, breakpoint_addr, BREAKPOINT_OPCODE);
         } else if (equals(debugger_command, "cont")) {
             // Make sure there is a process attached by the debugger
             if (working_pid == 0) {
@@ -174,7 +230,7 @@ int main( int argc, char *argv[] )  {
                 continue;
             }
 
-            long continue_ret_val = ptrace(PTRACE_CONT, working_pid, 0, 0);
+            QWORD continue_ret_val = ptrace(PTRACE_CONT, working_pid, 0, 0);
             if(continue_ret_val == -1) {
                 printf("[x] An error occured when trying to continue: %s\n", strerror(errno));
                 continue;
